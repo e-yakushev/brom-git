3e7de21c13014e40e91c459006aa0f80e9c75a45
Shamil Yakubov <shamilyakubov@hotmail.com>
2016-07-03 18:30:24 +0300

    v.1.1

--- code/brom-transport.F90
+++ code/brom-transport.F90
@@ -1,4 +1,4 @@
-! This file is part of Bottom RedOx Model (BROM, v.1.0).
+! This file is part of Bottom RedOx Model (BROM, v.1.1).
 ! BROM is free software: you can redistribute it and/or modify it under
 ! the terms of the GNU General Public License as published by the Free
 ! Software Foundation (https://www.gnu.org/licenses/gpl.html).
