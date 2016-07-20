d3a8f7dd56d4dd30afe85cd7d623b92ab40258a3
shamil <shamilyakubov@hotmail.com>
2016-07-20 10:13:45 +0300

    Relink FABM_NIVA_BASE

--- build_brom.sh
+++ build_brom.sh
@@ -4,4 +4,5 @@ mkdir -p build
 rm -r build/*
 CWD=`pwd`
 cp data/* build &&
-cd build && cmake $CWD/code -DFABM_BASE=$FABMDIR -DCMAKE_BUILD_TYPE=Debug
+cd build && cmake $CWD/code -DFABM_BASE=$FABMDIR \
+-DFABM_NIVA_BASE=$CWD/../fabm-brom-git -DCMAKE_BUILD_TYPE=Debug
